deb9082b8c33cc5f23641e3da7c0489c92c420f4
wiryosaputra <wiryo.saputro@tokopedia.com>
2016-11-02 09:40:18 +0000

    adding loop post

--- index.php
+++ index.php
@@ -1,37 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <?php wp_head();?>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <nav class="navbar navbar-default">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">
-            <img alt="Brand" src="<?php images('logo.png');?>" height="35">
-          </a>
-        </div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
+    <?php get_header();?>
+    <?php 
+    if(have_posts()):while(have_posts()):the_post();
+    ?>
+    <div class="container">
+        <h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
+        <?php the_content();?>
+    </div>
     
-    <?php wp_footer();?>
-  </body>
-</html>
+    <?php endwhile;endif;?>
+    <?php get_footer(); ?>
