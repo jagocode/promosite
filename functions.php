1c752ea004a6605f023d0978f9298166eb580d7e
wiryosaputra <wiryo.saputro@tokopedia.com>
2016-11-02 08:53:13 +0000

    adding css to wordpress

--- functions.php
+++ functions.php
@@ -4,4 +4,10 @@ function images($images){
     echo get_template_directory_uri().'/images/'.$images;
 }
 
+
+function promo_scripts(){
+    //add style to wordpress
+    wp_enqueue_style('stylecss',get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts','promo_scripts');
 ?>
