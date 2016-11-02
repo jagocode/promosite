c30fd3e9d8ef0fd17a5d5dede93e173556793436
wiryosaputra <wiryo.saputro@tokopedia.com>
2016-11-02 09:43:57 +0000

    adding comment template

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <div class="container">
         <h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
         <?php the_content();?>
+        <?php comments_template();?>
     </div>
     
     <?php endwhile;endif;?>
