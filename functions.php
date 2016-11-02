42cf88c21e9509eb0d21a6c77c75aa2460d1c204
wiryosaputra <wiryo.saputro@tokopedia.com>
2016-11-02 08:59:53 +0000

    adding bootstrap css

--- functions.php
+++ functions.php
@@ -6,8 +6,12 @@ function images($images){
 
 
 function promo_scripts(){
+    
+    //add boostrap style
+    wp_enqueue_style('bootstrap',get_template_directory_uri().'/css/bootstrap.min.css');
     //add style to wordpress
     wp_enqueue_style('stylecss',get_stylesheet_uri());
+    
 }
 add_action('wp_enqueue_scripts','promo_scripts');
 ?>
